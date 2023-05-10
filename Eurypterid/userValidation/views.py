52a95d40b14df25bff74cc7b175f9391a542489d
Pratham Gandhi <ppg.1382@gmail.com>
2023-05-10 19:31:15 +0530

    reverted some things

--- Eurypterid/userValidation/views.py
+++ Eurypterid/userValidation/views.py
@@ -28,30 +28,44 @@ def aadhar_upload(request):
             gray_blur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             _, thresh = cv2.threshold(gray_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
             text = pytesseract.image_to_string(thresh, lang='eng+tam+hin+tel+mal')
-
+            print(text)
             if 'male' in text.lower():
                 data['gender']= 'MALE'
             elif 'female' in text.lower():
                 data['gender'] = 'FEMALE'
 
-            lines = text.split('\n')
-            lines = list(filter(None, lines))
-            alphanumeric_pattern = re.compile("[A-Za-z0-9 ]+")
-            valid_lines = []
-            for line in lines:
-                if alphanumeric_pattern.fullmatch(line) is not None:
-                    valid_lines.append(line)
-            if len(valid_lines) >= 2:
-                data['name'] = valid_lines[0]
-                data['aadhar_no'] = valid_lines[1]
             data['dob'] = re.findall(r'[0-9]{2}-[0-9]{2}-[0-9]{4}', text)
             datetime_str = ""
             if len(data['dob']) > 0:
                 format = "%d-%m-%Y"
-            data['dob'] = re.findall(r'[0-9]{2}\/[0-9]{2}\/[0-9]{4}', text)
-            if len(data['dob']) > 0:
+            else:
+                data['dob'] = re.findall(r'[0-9]{2}\/[0-9]{2}\/[0-9]{4}', text)
                 format = "%d/%m/%Y"
+            if len(data['dob']) > 0:
                 datetime_str = datetime.datetime.strptime(data['dob'][0], format).date()
+
+            lines = text.split('\n')
+            space_rex = re.compile("[ ]*")
+            space_removed_lines = []
+            for line in lines:
+                if space_rex.fullmatch(line) is None:
+                    space_removed_lines.append(line)
+            alphanumeric_pattern = re.compile("[A-Za-z0-9 ]+")
+            valid_lines_temp = []
+            for line in space_removed_lines:
+                if alphanumeric_pattern.fullmatch(line) is not None:
+                    valid_lines_temp.append(line)
+            valid_lines = []
+            for line in valid_lines_temp:
+                if "government" in line.lower() or 'india' in line.lower():
+                    continue 
+                else:
+                    valid_lines.append(line)
+
+            if len(valid_lines) >= 2:
+                data['name'] = valid_lines[0]
+                data['aadhar_no'] = valid_lines[1]
+
             form = AadharUploadForm(initial={'name' : data['name'], 'dob' : datetime_str, 'aadhar_no' : data['aadhar_no'], 'gender' : data['gender']})
             response = render(request, 'aadhar_details.html', {'form' : form})
             response.set_cookie('name', data['name'])
